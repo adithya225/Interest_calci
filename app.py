74a5f87d8aac97ec838f69c6153816b707e06ecc
adithya <58217525+adithya225@users.noreply.github.com>
2021-12-21 11:44:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def c_interest(p,r,s_date,e_date):
     rt=(r*12)/100
     start_date = list(map(int, s_date.split('/')))
     end_date = list(map(int, e_date.split('/')))
+    print(start_date,end_date)
     sd, sm, sy = start_date[0], start_date[1], start_date[2]
     ed, em, ey = end_date[0], end_date[1], end_date[2]
     if ed<sd:
@@ -160,8 +161,11 @@ def predict():
     For rendering results on HTML GUI
     '''
     features = [str(x) for x in request.form.values()]
+    print(features)
     p = int(features[0])
+    print(p)
     r = float(features[1])
+    print(r)
     s_date = str(features[2])
     e_date = str(features[3])
     c_prediction = c_model(p,r,s_date,e_date)
@@ -176,14 +180,14 @@ def predict():
     end = e_date
     out1 = [principle, rate, start, end, d_output, s_output, s_output+p, c_output-p, c_output]
     s1 = "1) Principle Amount is: {0}".format(out1[0])
-    s2 = "2) Rate of Interest is: {0}".format(out1[1])
+    s2 = "2) Rate of Interest is: {0} x 12 %".format(out1[1])
     s3 = "3) Start Date: {0}".format(out1[2])
     s4 = "4) End Date: {0}".format(out1[3])
     s5 = "5) Total Number of Days is:  {0}".format(out1[4])
-    s6 = "6) Simple Interest for the Amount is: {0}".format(out1[5])
-    s7 = "7) Final Amount with respect to Simple Interest is: {}".format(out1[6])
-    s8 = "8) Compound Interest for the Amount is: {}".format(out1[7])
-    s9 = "9) Final Amount with respect to Compound Interest is: {}".format(out1[8])
+    s6 = "6) Interest for the Amount with respect to SI is: {0}".format(out1[5])
+    s7 = "7) Final Amount with respect to SI is: {}".format(out1[6])
+    s8 = "8) Interest for the Amount with respect to CI is: {}".format(out1[7])
+    s9 = "9) Final Amount with respect to CI is: {}".format(out1[8])
     return render_template('index.html', prediction_text1=s1, prediction_text2=s2,prediction_text3=s3, prediction_text4=s4,prediction_text5=s5, prediction_text6=s6,prediction_text7=s7, prediction_text8=s8,prediction_text9=s9)
 
 
