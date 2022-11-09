179bb6c7525c362e465f6ca340a5ce6731e9590f
adithya <58217525+adithya225@users.noreply.github.com>
2022-11-09 17:06:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="https://interest-calci-oaut.herokuapp.com/callback"
 )
 def login_is_required(function):
     def wrapper(*args, **kwargs):
